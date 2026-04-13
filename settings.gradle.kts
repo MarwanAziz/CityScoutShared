aecd5fa1252c80ed2691a8731544e6aed23c91a6
Marwan Aziz <marwan@marwanaziz.net>
2026-04-13 20:25:00 +0100

    fix building issue

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,17 @@
 rootProject.name = "CityScoutShared"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
+// JitPack currently hosts KMP metadata under *-metadata.jar while Gradle module files reference *.jar (404).
+// When ../CityScoutRemote exists, substitute the published dependency with the local :shared module.
+val cityScoutRemoteDir = file("../CityScoutRemote")
+if (cityScoutRemoteDir.resolve("settings.gradle.kts").isFile) {
+    includeBuild(cityScoutRemoteDir) {
+        dependencySubstitution {
+            substitute(module("com.github.MarwanAziz.CityScoutRemote:CityScoutRemote")).using(project(":shared"))
+        }
+    }
+}
+
 pluginManagement {
     repositories {
         google {
