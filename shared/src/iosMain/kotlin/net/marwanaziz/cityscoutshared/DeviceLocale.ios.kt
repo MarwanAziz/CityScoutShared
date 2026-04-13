7565b934fbadd074ba202946db977375aa606c60
Marwan Aziz <marwan@marwanaziz.net>
2026-04-13 20:24:45 +0100

    fix locale check

--- shared/src/iosMain/kotlin/net/marwanaziz/cityscoutshared/DeviceLocale.ios.kt
+++ shared/src/iosMain/kotlin/net/marwanaziz/cityscoutshared/DeviceLocale.ios.kt
@@ -1,17 +1,9 @@
 package net.marwanaziz.cityscoutshared
 
-import platform.Foundation.NSUserDefaults
+import platform.Foundation.NSLocale
+import platform.Foundation.autoupdatingCurrentLocale
+import platform.Foundation.countryCode
 
 actual fun isImperialLocale(): Boolean {
-    val localeTag = NSUserDefaults.standardUserDefaults.stringForKey("AppleLocale").orEmpty()
-    val countryCode = extractCountryCode(localeTag)
-    return countryCode == "US" || countryCode == "LR" || countryCode == "MM"
-}
-
-private fun extractCountryCode(localeTag: String): String {
-    if (localeTag.isEmpty()) return ""
-    val normalized = localeTag.replace('-', '_')
-    val parts = normalized.split('_')
-    if (parts.size < 2) return ""
-    return parts[1].uppercase()
+    return NSLocale.autoupdatingCurrentLocale.countryCode in setOf("US", "LR", "MM")
 }
